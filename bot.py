550e8fdb2f60d0936fca884991f5c459e7fb259c
sobhanjoon06 <sobhanjoon06@gmail.com>
2026-08-08 02:11:44 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,13 @@
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
 
+TOKEN = "8704293303:AAE1nlIFVXAMW7OGm1Jgebcj59dOmvVuCKY"
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("سلام 👋 به Pixsim خوش آمدید 🎬")
+
+app = Application.builder().token(TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+
+app.run_polling()
